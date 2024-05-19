605d78a3823e51b6d8bd10d2209b3e3ef9217787
Dunarintu Dragos-Gabriel <116965400+Dragos-Gabriel@users.noreply.github.com>
2024-05-19 08:20:00 +0300

    Add files via upload

---
+++ Migrations/20240519051707_TwoCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aplicatie.Migrations
+{
+    /// <inheritdoc />
+    public partial class TwoCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
